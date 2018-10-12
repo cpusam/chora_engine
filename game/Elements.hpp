3d0f905dd6d6d4ca4235e789295379aabdeba141
Samuel Leonardo <samuel.leonardo19@gmail.com>
2018-10-12 18:38:58 -0300

    Corrigido definição em Elements.hpp

--- game/Elements.hpp
+++ game/Elements.hpp
@@ -75,7 +75,7 @@ class Elements
 		Camera * currCamera;
 		std::vector<EntityID> entitiesID;
 		std::vector<Entity *> entities;
-		#if defined(WIN32) || defined(WIN64) || NO_THREAD_SAFE
+		#if defined(WIN32) || defined(WIN64) || defined(NO_THREAD_SAFE)
 		static Elements * singleton;
 		#else
 		static std::atomic<Elements *> singleton;
